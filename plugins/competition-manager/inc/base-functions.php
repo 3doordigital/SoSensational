85a2555a258e74943e6e074a81b2016dc9f52630
dantaylorseo <dan@tailored.im>
2015-03-24 14:06:31 +0000

    Fix fatal error

--- plugins/competition-manager/inc/base-functions.php
+++ plugins/competition-manager/inc/base-functions.php
@@ -1,4 +1,6 @@
 <?php
-	function print_var($var) {
-        echo '<pre>'.print_r($var, true).'</pre>';   
-    }
+	if( !function_exists( 'print_var' ) ) {
+		function print_var($var) {
+        	echo '<pre>'.print_r($var, true).'</pre>';   
+    	}
+	}
