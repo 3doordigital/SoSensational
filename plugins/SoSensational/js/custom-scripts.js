6d1a03e445901618242fa63f14d672ca5ed1794b
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2015-03-25 17:05:32 +0100

    Fix video embed on advertiset page

--- plugins/SoSensational/js/custom-scripts.js
+++ plugins/SoSensational/js/custom-scripts.js
@@ -151,5 +151,20 @@ jQuery(document).ready(function($) {
         start: function() {
             $('.slides').show();
         }        
-    }); 
+    });
+    
+    /*--------------------------------------------------------------------------
+     A function that extracts video ID from a youtube link to use in the embed
+     -------------------------------------------------------------------------*/
+    
+    function getId(url) {
+        var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+        var match = url.match(regExp);
+
+        if (match && match[2].length == 11) {
+            return match[2];
+        } else {
+            return 'error';
+        }
+    }    
 });
