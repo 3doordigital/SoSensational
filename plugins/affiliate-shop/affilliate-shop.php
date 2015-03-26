74c94397538d6a9585a64f7eacc6f7d803a6d428
dantaylorseo <dan@tailored.im>
2015-03-26 12:25:18 +0000

    testing

--- plugins/affiliate-shop/affilliate-shop.php
+++ plugins/affiliate-shop/affilliate-shop.php
@@ -1361,8 +1361,11 @@ class WordPress_Affiliate_Shop {
                    </div>
                   
                   </div>
-                   <?php   $i++; } } ?>
-                         
+                   <?php   $i++; } } else { ?>
+                         <div class="postbox">
+                         	<p>No products, <a href="<?php echo admin_url( 'admin.php?page=affiliate-shop/add-products' ); ?>">please go back and add more</a></p>
+                         </div>
+                   <?php } ?>
                   </div>
                   </div>
             </form>
