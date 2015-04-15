ecc6a58cdfb4243522a84811ecea6a83666c7324
@dantaylorseo <dan@tailored.im>
2015-04-15 14:36:08 +0100

    New/Sale/Picks Options

--- plugins/affiliate-shop/templates/shop-template.php
+++ plugins/affiliate-shop/templates/shop-template.php
@@ -124,16 +124,16 @@ get_header();
 					switch ( $wp_query->query_vars['shop-option'] ) {
 						case 'new' :
 							echo '<h1>'.( isset( $option['faceted']['newin']['title'] ) ? $option['faceted']['newin']['title'] : 'New In' ).'</h1>';
-							echo wpautop(htmlspecialchars_decode( $option['faceted']['newin']['intro'] ));
+							if( isset( $option['faceted']['newin']['intro'] ) ) echo wpautop(htmlspecialchars_decode( $option['faceted']['newin']['intro'] ));
 							break;
 						case 'sale' :
 							echo '<h1>'.( isset( $option['faceted']['sale']['title'] ) ? $option['faceted']['sale']['title'] : 'Sale Items' ).'</h1>';
-							echo wpautop(htmlspecialchars_decode( $option['faceted']['sale']['intro'] ));
+							if( isset( $option['faceted']['shop']['intro'] ) ) echo wpautop(htmlspecialchars_decode( $option['faceted']['sale']['intro'] ));
 							break;
 							
 						case 'picks' :
 							echo '<h1>'.( isset( $option['faceted']['picks']['title'] ) ? $option['faceted']['picks']['title'] : 'Top Picks' ).'</h1>';
-							echo wpautop(htmlspecialchars_decode( $option['faceted']['picks']['intro'] ));
+							if( isset( $option['faceted']['picks']['intro'] ) ) echo wpautop(htmlspecialchars_decode( $option['faceted']['picks']['intro'] ));
 							break;	
 					}
 				}
