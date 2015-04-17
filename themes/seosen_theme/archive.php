a263c2ef00c1e84e0b64d9b330e9f2ea00c1b2e5
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2015-04-17 17:36:16 +0200

    Apply formatting, fix bb categorires tiles

--- themes/seosen_theme/archive.php
+++ themes/seosen_theme/archive.php
@@ -1,21 +1,25 @@
 <?php get_header(); ?>
 <div class="container">
     <h2 class="pagetitle"><span>Style Advice &amp; Blog</span></h2>
-    <?php if ( function_exists('wp_nav_menu') ) { wp_nav_menu( array(
-                'menu'              => 'blog',
-                'theme_location'    => 'blog',
-                'container'         => 'div',
-                'container_class'   => '',
-        		'container_id'      => 'topcats',
-                'menu_class'        => '',
-                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                'walker'            => new wp_bootstrap_navwalker()
-				)
-            ); } ?>
-    <?php 
-        if ( function_exists('yoast_breadcrumb') ) {
-            yoast_breadcrumb('<div id="breadcrumbs">','</div>');
-        } 
+    <?php
+    if (function_exists('wp_nav_menu')) {
+        wp_nav_menu(array(
+            'menu' => 'blog',
+            'theme_location' => 'blog',
+            'container' => 'div',
+            'container_class' => '',
+            'container_id' => 'topcats',
+            'menu_class' => '',
+            'fallback_cb' => 'wp_bootstrap_navwalker::fallback',
+            'walker' => new wp_bootstrap_navwalker()
+            )
+        );
+    }
+    ?>
+    <?php
+    if (function_exists('yoast_breadcrumb')) {
+        yoast_breadcrumb('<div id="breadcrumbs">', '</div>');
+    }
     ?>
 </div>
 <div class="container">
@@ -23,72 +27,77 @@
         <div class="col-md-16" id="content">
             <h1 class="cattitle"><?php single_cat_title('', true); ?></h1>
             <?php
-                $i = 1;
-                $x = 0;
-                if( have_posts() ) : while( have_posts() ) : the_post();
-            ?>
-            
-            <?php if( $i ==1 ) { ?>
-                <div class="row">
-                    <div class="col-md-24">
-                    	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                        <?php sosen_post_meta(); ?>
-                        <?php 
-                            if ( has_post_thumbnail( ) ) { // check if the post has a Post Thumbnail assigned to it.
-                                 ?>
-                                <a href="<?php the_permalink(); ?>">
-                                <?php
-								the_post_thumbnail( 'blog-large', array( 'class' => 'img-responsive' ) );
-								?>
-                                </a>
-                                <?php
-                            } 
-                        ?>
-                        
+            $i = 1;
+            $x = 0;
+            if (have_posts()) : while (have_posts()) : the_post();
+                    ?>
+
+                            <?php if ($i == 1) { ?>
+                        <div class="row">
+                            <div class="col-md-24">
+                                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                                    <?php sosen_post_meta(); ?>
+                                    <?php
+                                    if (has_post_thumbnail()) { // check if the post has a Post Thumbnail assigned to it.
+                                        ?>
+                                    <a href="<?php the_permalink(); ?>">
+                                    <?php
+                                    the_post_thumbnail('blog-large', array('class' => 'img-responsive'));
+                                    ?>
+                                    </a>
+                                    <?php
+                                }
+                                ?>
+
                         <?php
-                            the_excerpt_max_charlength(340);
+                        the_excerpt_max_charlength(340);
                         ?>
-                    </div>
-                </div>
-            <?php } else { ?>
-                <?php if( $x == 0 ) { ?>
-                    <div class="row">
-                <?php } ?>
-                    <div class="col-md-12 blog-small">
-                    	
-                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                        <?php sosen_post_meta(); ?>
-                        <?php 
-                            if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-                                 ?>
-                                <a href="<?php the_permalink(); ?>">
-                                <?php
-								the_post_thumbnail( 'blog-small', array( 'class' => 'img-responsive' ) );
-								?>
-                                </a>
-                                <?php
-                            } 
-                        ?>
-                        
-                        <?php
+                            </div>
+                        </div>
+        <?php } else { ?>
+            <?php if ($x == 0) { ?>
+                            <div class="row">
+                                <?php } ?>
+                            <div class="col-md-12 blog-small">
+
+                                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                                    <?php sosen_post_meta(); ?>
+                                    <?php
+                                    if (has_post_thumbnail()) { // check if the post has a Post Thumbnail assigned to it.
+                                        ?>
+                                    <a href="<?php the_permalink(); ?>">
+                                    <?php
+                                    the_post_thumbnail('blog-small', array('class' => 'img-responsive'));
+                                    ?>
+                                    </a>
+                                    <?php
+                                }
+                                ?>
+
+                            <?php
                             the_excerpt_max_charlength(170);
-                        ?>
-                    </div>   
-                <?php if( $x == 1 || $wp_query->post_count == $i ) { ?>
-                    </div>
-                    
-                <?php } ?>
-                <?php if( $x == 1) { $x = 0; } else { $x++; } ?>
-            <?php  } 
-                $i++;
-            ?>
-            <?php endwhile; ?>
-            <?php wp_pagenavi(); ?>
-            <?php endif; ?>
+                            ?>
+                            </div>   
+                        <?php if ($x == 1 || $wp_query->post_count == $i) { ?>
+                            </div>
+
+                        <?php } ?>
+                        <?php if ($x == 1) {
+                            $x = 0;
+                        } else {
+                            $x++;
+                        } ?>
+                    <?php
+                    }
+                    $i++;
+                    ?>
+    <?php endwhile; ?>
+    <?php wp_pagenavi(); ?>
+<?php endif; ?>
         </div>
         <div class="col-md-1"></div>
         <div class="col-md-7">
-            <?php dynamic_sidebar( 'blog_sidebar' ); ?> 
+<?php dynamic_sidebar('blog_sidebar'); ?> 
         </div>
     </div>
 </div>
