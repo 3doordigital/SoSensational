d9fcf3822e9bee3d0f7f80c4cac5fbecdefeaf9d
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2015-04-27 15:27:45 +0200

    Add multiple accounts plugin

--- plugins/SoSensational/web/view-category.php
+++ plugins/SoSensational/web/view-category.php
@@ -60,8 +60,8 @@ if (!empty($ss_cat_id)):
                     </a>
                 </div>
                 <?php
-            endif;
-            $counterColor++;
+                $counterColor++;
+            endif;            
         endforeach;
         ?>
         <div class="ss_clear" ></div>
