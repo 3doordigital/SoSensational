296731e30c8550a32445c3770cfab8ddfc055559
@dantaylorseo <dan@tailored.im>
2015-04-29 17:32:46 +0100

    shop update IDs

--- plugins/affiliate-shop/classes/class.api.php
+++ plugins/affiliate-shop/classes/class.api.php
@@ -390,9 +390,9 @@
 				$oRefineByDefinition->sName = '';
 				
 				$oRefineBy->oRefineByDefinition = $oRefineByDefinition;
-				$title = str_replace( array( '-', '*', ',' ), '', $title );
-				//$title = htmlspecialchars( $title );
-				//$title = $title[0];
+				$title = str_replace( array( '-', '*', ',', '%5C', '%27' ), '', $title );
+				$title = explode( "'", $title );
+				$title = $title[0];
 				$params = array("sQuery" => stripslashes($title), "iLimit" => 1, "bAdult" => false, 'sColumnToReturn' => array('sAwImageUrl', 'sMerchantImageUrl', 'sBrand', 'sDescription', 'fRrpPrice' ),  "oActiveRefineByGroup"	=>	$oRefineBy);
 				//print_var($params);
 				$client = ClientFactory::getClient();
