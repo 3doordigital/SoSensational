5c5abbe8786c2222545744012d1c19ab2c0f7a3d
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2015-05-04 09:22:47 +0200

    Remove var_dump

--- themes/seosen_theme/header.php
+++ themes/seosen_theme/header.php
@@ -10,7 +10,6 @@
     </head>
     <body <?php body_class(); ?>>
         <div class="container" id="topbar">
-            <?php global $query_string; var_dump($query_string); ?>
             <div class="row">
                 <div class="blog-masthead">
                     <div class="col-xs-24 col-md-16"><span>SHOPPING, STYLE AND BEAUTY FOR GROWN UP WOMEN</span></div>                
