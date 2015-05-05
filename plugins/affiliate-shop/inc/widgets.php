1bea10e421086f3c2468ec9d5bb6b14b3cd95447
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2015-05-05 11:16:19 +0200

    Responsive shop filters

--- plugins/affiliate-shop/inc/widgets.php
+++ plugins/affiliate-shop/inc/widgets.php
@@ -22,7 +22,7 @@ class aff_category_widget extends WP_Widget {
         global $wp_query;
         echo $args['before_widget'];
         if ( ! empty( $instance['title'] ) ) {
-            echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . '<i class="fa fa-level-down navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"></i>' . $args['after_title'];
+            echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . '<i class="fa fa-level-down navbar-toggle collapsed visible-xs" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"></i>' . $args['after_title'];
         }
         if( isset( $wp_query->query_vars['shop-cat'] ) ) {
             $term = get_term_by( 'slug', $wp_query->query_vars['shop-cat'], 'wp_aff_categories' );
@@ -35,7 +35,7 @@ class aff_category_widget extends WP_Widget {
         }
         echo '<div class="wp_aff_categories navbar navbar-default">';                
             echo '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">';
-                echo '<ul class="nav navbar-nav">';
+                echo '<ul class="nav navbar-nav shop-filter">';
 
                 global $wp_aff;
                 $fn_include = $wp_aff->get_product_terms('wp_aff_categories');
