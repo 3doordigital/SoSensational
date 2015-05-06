d8f6151cd14397aaeb4275160632e01cf1499228
@dantaylorseo <dan@tailored.im>
2015-05-06 16:29:19 +0100

    Homepage invision

--- themes/seosen_theme/inc/template_functions.php
+++ themes/seosen_theme/inc/template_functions.php
@@ -2,12 +2,12 @@
 
 	function new_excerpt_more($more) {
 	   global $post;
-	   return '';
+	   return '...';
 	}
 	add_filter('excerpt_more', 'new_excerpt_more');
 	
 	function custom_excerpt_length( $length ) {
-		return 15;
+		return 18;
 	}
 	add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 	
