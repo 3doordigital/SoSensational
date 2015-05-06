dd8022b4485f9d6b5e7653c9a3c652d93a6c87c6
@dantaylorseo <dan@tailored.im>
2015-05-06 16:28:49 +0100

    Homepage invision

--- themes/seosen_theme/inc/template_functions.php
+++ themes/seosen_theme/inc/template_functions.php
@@ -2,7 +2,7 @@
 
 	function new_excerpt_more($more) {
 	   global $post;
-	   return ' <a class="moretag" href="'. get_permalink($post->ID) . '">Read more...</a>';
+	   return '';
 	}
 	add_filter('excerpt_more', 'new_excerpt_more');
 	
