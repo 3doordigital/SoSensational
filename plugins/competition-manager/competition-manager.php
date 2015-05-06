f28eed19f1129dc165b1660c1b40a9a9280f3de3
@dantaylorseo <dan@tailored.im>
2015-05-06 13:40:18 +0100

    Shop Update

--- plugins/competition-manager/competition-manager.php
+++ plugins/competition-manager/competition-manager.php
@@ -807,7 +807,7 @@ class WordPress_Competition_Manager {
 				echo '<input type="hidden" name="competition" value="'.get_the_title().'">';
 				echo '<input type="hidden" name="competition-id" value="'.get_the_ID().'">';
 				echo '<input type="hidden" name="action" value="wp_comp_man_add_entry">';
-				echo '<a href="/competition-terms-conditions/">Terms &amp; Conditions</a>';
+				echo '<a target="_blank" href="/competition-terms-conditions/">Terms &amp; Conditions</a>';
 				echo'<p><button type="submit" class="btn btn-primary" id="submit_answer">Submit Answer</button></p>';
 				echo '</form>';
 				return ob_get_contents();
