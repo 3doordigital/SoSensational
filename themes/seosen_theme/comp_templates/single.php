bd87a5e39aac7031af39e52f16105d987c2e3da1
@dantaylorseo <dan@tailored.im>
2015-05-06 13:47:08 +0100

    Shop Update

--- themes/seosen_theme/comp_templates/single.php
+++ themes/seosen_theme/comp_templates/single.php
@@ -15,6 +15,11 @@
 <p class="center"><strong><?php $meta['wp_comp_rules'][0]; ?></strong></p>
 <p class="center"><?php printf('This competition begins on %s and ends at midnight on %s', date( 'jS F Y', strtotime( $meta['wp_comp_sdate'][0] ) ), date( 'jS F Y', strtotime( $meta['wp_comp_edate'][0] ) ) ) ; ?></p>
 <p class="center"><button type="button" class="btn btn-default btn-lg show_comp">Enter Now</button></p>
+<ul>
+	<li>Please note that automated entries, bulk entries or third party entries will be disqualified</li>
+	<li>By entering this competition, you are consenting to receive information and newsletters from SoSensational. You can unsubscribe at any time.</li>
+	<li>Competition hosting site <a href="http://www.theprizefinder.com/" target="_blank" rel="nofollow">ThePrizeFinder.com</a></li>
+</ul>
 <div id="wp_comp_form" style="display:none;">
 	<?php
 		$comp_manager->frontend_form( true, 24 );
