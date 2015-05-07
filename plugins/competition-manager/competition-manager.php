e2e88e01fcd56357b8a42f5f73b7d1422ab2ec46
@dantaylorseo <dan@tailored.im>
2015-05-07 13:30:14 +0100

    Last fixes

--- plugins/competition-manager/competition-manager.php
+++ plugins/competition-manager/competition-manager.php
@@ -811,7 +811,7 @@ class WordPress_Competition_Manager {
 				$captcha_instance = new ReallySimpleCaptcha();
 				$word = $captcha_instance->generate_random_word();
 				$prefix = mt_rand();
-				$captcha_instance->generate_image( $prefix, $word );
+				echo $captcha_instance->generate_image( $prefix, $word );
 				
 				
 				echo '<a target="_blank" href="/competition-terms-conditions/">Terms &amp; Conditions</a>';
