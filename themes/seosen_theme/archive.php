7465652990101fa609e201fce66bb07a0685f80a
@dantaylorseo <dan@tailored.im>
2015-05-11 09:32:19 +0100

    Font Fixes

--- themes/seosen_theme/archive.php
+++ themes/seosen_theme/archive.php
@@ -25,7 +25,7 @@
     <div id="breadcrumbs" class="breadcrumbs" xmlns:v="http://rdf.data-vocabulary.org/#">
 		<?php if(function_exists('bcn_display'))
         {
-            //bcn_display();
+            bcn_display();
         }?>
     </div>
 </div>
