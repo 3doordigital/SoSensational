ce780afdb140d9723428c0564d14ea67f127c99d
@dantaylorseo <dan@tailored.im>
2015-05-11 14:43:23 +0100

    B&B Fix

--- plugins/SoSensational/classes/RelatedCarousel.php
+++ plugins/SoSensational/classes/RelatedCarousel.php
@@ -85,7 +85,7 @@ class RelatedCarousel
                                                                                     true 
                                                                                     ); 
             
-          if( isset($advertiserTmp[$this->dataForDisplay[$advertiserCategory->post_title]['advertiser'][0] ) )
+          if( isset($advertiserTmp[$this->dataForDisplay[$advertiserCategory->post_title]['advertiser'][0]] ) )
 		  {                                                                                                                              
             // Skip an iteration of the loop if the advertiser has already been displayed                                   
             if ( ! isset($advertiserTmp[$this->dataForDisplay[$advertiserCategory->post_title]['advertiser'][0]->post_title]) ) {
