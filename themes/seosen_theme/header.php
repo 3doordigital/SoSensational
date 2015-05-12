84ea87c4768918ab4c467e168cf7951407e98d53
Przemyslaw Olesinski <przemek.4mm@gmail.com>
2015-05-12 08:31:30 +0200

    viewport as 0.9

--- themes/seosen_theme/header.php
+++ themes/seosen_theme/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?> class="no-js">
     <head>
         <meta charset="<?php bloginfo('charset'); ?>">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=0.9">
         <link rel="profile" href="http://gmpg.org/xfn/11">
         <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
         <title><?php wp_title(''); ?></title>
