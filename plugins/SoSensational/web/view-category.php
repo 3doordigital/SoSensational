1d06ae24e6b73f878dca1372ddbe0b3ab8aff2d8
Przemyslaw Olesinski <przemek.4mm@gmail.com>
2015-05-13 08:15:56 +0200

    add row to featued brands and margin

--- plugins/SoSensational/web/view-category.php
+++ plugins/SoSensational/web/view-category.php
@@ -77,45 +77,47 @@ if (!empty($ss_cat_id)):
         if(sizeof($featureds) > 0 ):
     ?>
         
-        <div class="featured-brand">
+        <div class="featured-brand row">
             <div class="line">
                 <h2>Featured Brands</h2>
                 <span></span>
             </div>
-            <?php 
-                $counterCategories = 1;
-                $counterColor = 1;
-                foreach ($featureds as $featured):
-            ?>
-                <div class="col-md-8 col-sm-12 fadebox showme animated fadeIn category-picture-tile" style="visibility: visible;">
-                <a href="<?php echo get_permalink($featured->ID); ?>" class="aHolderImgSS">
-                        <?php 
-                            $featured_child = $wpdb->get_results("SELECT * FROM {$wpdb->posts} wpp
-                                                                                  LEFT JOIN {$wpdb->term_relationships} as wptr
-                                                                                  ON wptr.object_id = wpp.ID
-                                                                                  LEFT JOIN {$wpdb->term_taxonomy} as wptt
-                                                                                  ON wptr.term_taxonomy_id = wptt.term_taxonomy_id
-                                                                                  WHERE wpp.post_type = 'advertisers_cats'
-                                                                                  AND wpp.post_parent = ".$featured->ID."
-                                                                                  AND wptt.parent = ".$category_id."
-                                                                                  ORDER BY RAND()
-                                                                                  LIMIT 1
-                                                                                ", OBJECT);     
-                        ?>                        
-                        <img  src="<?php echo get_post_meta($featured_child[0]->ID, 'ss_advertisers_cats_image')[0]; ?>" class="img-responsive" />
-                        <div class="<?php
-                        if ($counterColor % 2): echo 'whitebar ss_whitebar';
-                        else: echo 'blackbar ss_blackbar';
-                        endif;
-                        ?>" style="display:block">
-                            <h2><span> <?php echo $featured->post_title; ?></span></h2>
-                        </div>
-                    </a>
-                </div>
-            <?php 
-                $counterColor++; 
-                endforeach;
-            ?>
+            <div class="featued-list">
+                <?php 
+                    $counterCategories = 1;
+                    $counterColor = 1;
+                    foreach ($featureds as $featured):
+                ?>
+                    <div class="col-md-8 col-sm-12 fadebox showme animated fadeIn category-picture-tile" style="visibility: visible;">
+                    <a href="<?php echo get_permalink($featured->ID); ?>" class="aHolderImgSS">
+                            <?php 
+                                $featured_child = $wpdb->get_results("SELECT * FROM {$wpdb->posts} wpp
+                                                                                      LEFT JOIN {$wpdb->term_relationships} as wptr
+                                                                                      ON wptr.object_id = wpp.ID
+                                                                                      LEFT JOIN {$wpdb->term_taxonomy} as wptt
+                                                                                      ON wptr.term_taxonomy_id = wptt.term_taxonomy_id
+                                                                                      WHERE wpp.post_type = 'advertisers_cats'
+                                                                                      AND wpp.post_parent = ".$featured->ID."
+                                                                                      AND wptt.parent = ".$category_id."
+                                                                                      ORDER BY RAND()
+                                                                                      LIMIT 1
+                                                                                    ", OBJECT);     
+                            ?>                        
+                            <img  src="<?php echo get_post_meta($featured_child[0]->ID, 'ss_advertisers_cats_image')[0]; ?>" class="img-responsive" />
+                            <div class="<?php
+                            if ($counterColor % 2): echo 'whitebar ss_whitebar';
+                            else: echo 'blackbar ss_blackbar';
+                            endif;
+                            ?>" style="display:block">
+                                <h2><span> <?php echo $featured->post_title; ?></span></h2>
+                            </div>
+                        </a>
+                    </div>
+                <?php 
+                    $counterColor++; 
+                    endforeach;
+                ?>
+            </div>    
         </div>
 
     <?php endif; ?>
