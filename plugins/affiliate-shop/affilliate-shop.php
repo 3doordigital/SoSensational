cf8016f6388bb25871cb4708c179d4a0698d8a0d
@dantaylorseo <dan@tailored.im>
2015-05-14 18:12:35 +0100

    Shop sitemaps

--- plugins/affiliate-shop/affilliate-shop.php
+++ plugins/affiliate-shop/affilliate-shop.php
@@ -2655,7 +2655,7 @@ class WordPress_Affiliate_Shop {
 		foreach( $terms as $term ) {
 			
 			echo '<url>
-		<loc>'.site_url().'/shop/'.$term->slug.'/</loc>
+		<loc>'.site_url().'/shop/'.( $type == 'brands' ? 'brand/' : '' ).$term->slug.'/</loc>
 		<lastmod>'.$this->get_last_post_date( $term, $tax ).'</lastmod>
 		<changefreq>daily</changefreq>
 		<priority>0.3</priority>
