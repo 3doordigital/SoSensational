4b3d176cbe45f508525574b0e0c97691b2b332e3
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2015-05-18 15:59:19 +0200

    Uncomment breadcrumbs script

--- plugins/SoSensational/js/custom-scripts.js
+++ plugins/SoSensational/js/custom-scripts.js
@@ -158,21 +158,19 @@ jQuery(document).ready(function($) {
     var urls = [];
     var mainCategory;
 
-    // was not working correct
-
-    // str.each(function(index) {
-    //     var url = $(this).attr('href');
-    //     urls.push(url);      
-    //     if (urls.length === 3) {
-    //         var slug = urls[2].slice(7);            
-    //         $(this).attr('href', urls[1] + slug);  
-    //         mainCategory = $(this).attr('href');
-    //     }         
-    //     if (urls.length === 4) {
-    //         var slug = urls[3].slice(7);    
-    //         $(this).attr('href', mainCategory + '/' + slug);                 
-    //     }
-    // });
+     str.each(function(index) {
+         var url = $(this).attr('href');
+         urls.push(url);      
+         if (urls.length === 3) {
+             var slug = urls[2].slice(7);            
+             $(this).attr('href', urls[1] + slug);  
+             mainCategory = $(this).attr('href');
+         }         
+         if (urls.length === 4) {
+             var slug = urls[3].slice(7);    
+             $(this).attr('href', mainCategory + '/' + slug);                 
+         }
+     });
     
     /*--------------------------------------------------------------------------
      Preview functionality on advertiser edit page
