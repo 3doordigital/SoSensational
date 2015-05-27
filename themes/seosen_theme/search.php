6ada44b057658b97fb55a72a07f299fe36d70e20
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2015-05-27 17:28:31 +0200

    Searchpage - fix function inclusion issue

--- themes/seosen_theme/search.php
+++ themes/seosen_theme/search.php
@@ -1,5 +1,3 @@
 <?php get_header(); ?>
-<?php get_template_part('search', 'shop'); ?>
-<?php get_template_part('search', 'bb'); ?>
-<?php get_template_part('search', 'blog'); ?>
-<?php get_footer(); ?>
+
+<?php get_footer();
