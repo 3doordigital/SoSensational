687b7742067312010e563f947e63763947d0f902
@dantaylorseo <dan@tailored.im>
2015-06-02 17:01:03 +0100

    cron

--- plugins/affiliate-shop-manager/cron.php
+++ plugins/affiliate-shop-manager/cron.php
@@ -1,6 +1,20 @@
 <?php
-	define('WP_USE_THEMES', false);
-	require($_SERVER['DOCUMENT_ROOT'].'/wp-blog-header.php');
+	function find_wordpress_base_path() {
+        $dir = dirname(__FILE__);
+        do {
+            //it is possible to check for other files here
+            if( file_exists($dir."/wp-config.php") ) {
+                return $dir;
+            }
+        } while( $dir = realpath("$dir/..") );
+        return null;
+    }
+
+    define( 'BASE_PATH', find_wordpress_base_path()."/" );
+    define('WP_USE_THEMES', false);
+    global $wp, $wp_query, $wp_the_query, $wp_rewrite, $wp_did_header;
+    require(BASE_PATH . 'wp-load.php');
+	
 	global $feed_man;
 
 	$feed_man->cron_process();
