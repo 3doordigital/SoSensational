5ce02828347ac01c17ad9b6e272625e9b1278dd0
@dantaylorseo <dan@tailored.im>
2015-06-02 14:29:53 +0100

    cron

--- plugins/affiliate-shop-manager/cron.php
+++ plugins/affiliate-shop-manager/cron.php
@@ -1,6 +1,6 @@
 <?php
 	define('WP_USE_THEMES', false);
 	require($_SERVER['DOCUMENT_ROOT'].'/wp-blog-header.php');
-	global $feed_man;
+	//global $feed_man;
 
-	$feed_man->cron_process();
+	//$feed_man->cron_process();
