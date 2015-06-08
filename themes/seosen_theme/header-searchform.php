900afd7f91fc93998eac2c7496147ba92bcd0924
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2015-06-08 15:21:52 +0200

    Hide autocmplete on the search input field

--- themes/seosen_theme/header-searchform.php
+++ themes/seosen_theme/header-searchform.php
@@ -2,7 +2,7 @@
 	<div class="form-group">	
         <div id="searchform-top">
             <label for="sosen-searchbox">Hi, I'm interested in:</label>
-            <input type="search" id="sosen-searchbox" class="search-field input form-control" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+            <input type="search" id="sosen-searchbox" class="search-field input form-control" autocomplete="off" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
             <span class="input-group-btn" id="btnSubmit">
                 <button type="submit" class="search-submit btn btn-primary"><i class="fa fa-chevron-right"></i></button>
             </span>
