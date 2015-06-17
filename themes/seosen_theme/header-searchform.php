be4910f265e512103945fda9a47ee31b62d49670
@dantaylorseo <dan@tailored.im>
2015-06-17 08:59:40 +0100

    remove search

--- themes/seosen_theme/header-searchform.php
+++ themes/seosen_theme/header-searchform.php
@@ -1,11 +1,12 @@
 <form role="search" method="get" class="search-form form-inline" action="<?php echo home_url( '/' ); ?>">
 	<div class="form-group">	
         <div id="searchform-top">
-            <label for="sosen-searchbox">Hi, I'm interested in:</label>
+            <!--<label for="sosen-searchbox">Hi, I'm interested in:</label>
             <input type="search" id="sosen-searchbox" class="search-field input form-control" autocomplete="off" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
             <span class="input-group-btn" id="btnSubmit">
                 <button type="submit" class="search-submit btn btn-primary"><i class="fa fa-chevron-right"></i></button>
-            </span>
+            </span>-->
+            <label for="sosen-searchbox">We're not quite dressed yet.</label>
         </div>
     </div>
 </form>
