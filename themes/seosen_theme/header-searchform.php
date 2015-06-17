554be50d2499afd078c99d0874288b7befbd6646
@dantaylorseo <dan@tailored.im>
2015-06-17 15:38:35 +0100

    search fix

--- themes/seosen_theme/header-searchform.php
+++ themes/seosen_theme/header-searchform.php
@@ -1,12 +1,12 @@
 <form role="search" method="get" class="search-form form-inline" action="<?php echo home_url( '/' ); ?>">
 	<div class="form-group">	
         <div id="searchform-top">
-            <!--<label for="sosen-searchbox">Hi, I'm interested in:</label>
+            <label for="sosen-searchbox">Hi, I'm interested in:</label>
             <input type="search" id="sosen-searchbox" class="search-field input form-control" autocomplete="off" placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
             <span class="input-group-btn" id="btnSubmit">
                 <button type="submit" class="search-submit btn btn-primary"><i class="fa fa-chevron-right"></i></button>
-            </span>-->
-            <label for="sosen-searchbox">Search isn't quite dressed yet, please come back soon.</label>
+            </span>
+            <!--<label for="sosen-searchbox">Search isn't quite dressed yet, please come back soon.</label>-->
         </div>
     </div>
 </form>
