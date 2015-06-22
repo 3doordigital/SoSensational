2972e6835ac8ef9d771ab097bc0004525b6017ae
@dantaylorseo <dan@tailored.im>
2015-06-22 10:16:42 +0100

    fix brands issue on shop

--- plugins/affiliate-shop/inc/widgets.php
+++ plugins/affiliate-shop/inc/widgets.php
@@ -120,7 +120,7 @@ class aff_brand_widget extends WP_Widget {
                 echo '<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-2">';
                     echo '<ul class="nav navbar-nav shop-filter">';
                         $walker = new Faceted_Brand_Walker;
-						echo $_SERVER['REQUEST_URI'];
+						
 						if( $_SERVER['REQUEST_URI'] == '/shop/' ) {
 							wp_list_categories( array( 'depth' => 0, 'taxonomy' => 'wp_aff_brands', 'hide_empty' => 0, 'walker' => $walker, 'title_li' => '', 'orderby' => 'name', 'hierarchical' => 1 ) );
 						} else {
