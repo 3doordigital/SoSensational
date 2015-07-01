d64f17706ff3f499853c748d5dc5130d8cb33edc
@dantaylorseo <dan@tailored.im>
2015-07-01 14:09:13 +0100

    linkshare price fix

--- plugins/affiliate-shop/classes/class.api.php
+++ plugins/affiliate-shop/classes/class.api.php
@@ -211,11 +211,11 @@
 				FROM 
 					$table_name 
 				WHERE product_id 
-					REGEXP '^{$merch}_{$prod_id}$' 
+					= '$prod_id' 
 				LIMIT 1
 				";
 			//$out = $query;
-			die( $query );
+			echo $query;
 			if ($products = $wpdb->get_results( $query, ARRAY_A	) ) {
 				foreach ( $products as $product ) 
 				{
