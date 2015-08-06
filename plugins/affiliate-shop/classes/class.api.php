92bd9fe931ae9f7167152eb6e9ef53ae7a70646b
@dantaylorseo <dan@tailored.im>
2015-08-06 12:51:35 +0100

    Hugo Boss

--- plugins/affiliate-shop/classes/class.api.php
+++ plugins/affiliate-shop/classes/class.api.php
@@ -47,7 +47,7 @@
 				WHERE MATCH(product_title) AGAINST('$search' IN BOOLEAN MODE)	";
 			//$query .= "LIKE '%$term%' ";
 			if( $api != 'all' ) { $query .= " AND product_aff='$api' "; }
-			if( $merchant != NULL && $merchant != 0 ) { $query .= " AND product_merch='$merchant' "; }	
+			if( $merchant !== 0 ) { $query .= " AND product_merch='$merchant' "; }	
 			$query .= "ORDER BY MATCH(product_title) AGAINST('$search' IN BOOLEAN MODE) DESC";
 			$query2 = $query." LIMIT $offset, $depth";
 			echo $merchant;
