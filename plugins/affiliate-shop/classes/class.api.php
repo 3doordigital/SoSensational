4419f4cdfd905473515a216830d0d3f625d444fa
Kamil Kosakowski <kamilk870310@wp.pl>
2015-10-07 12:36:26 +0200

    test search query

--- plugins/affiliate-shop/classes/class.api.php
+++ plugins/affiliate-shop/classes/class.api.php
@@ -52,7 +52,7 @@
 			}	
 			$query .= "ORDER BY MATCH(product_title) AGAINST('$search' IN BOOLEAN MODE) DESC";
 			$query2 = $query." LIMIT $offset, $depth";
-			
+			var_dump($query2);
 			//$out = $query;
 			//$totalres = $wpdb->get_results( $query );
 			
