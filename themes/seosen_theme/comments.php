82bb2e40659fa5443fd5d21bd6fbe9e64a4e5d60
Kamil Kosakowski <kamilk870310@wp.pl>
2015-10-14 14:36:52 +0200

    7 th test

--- themes/seosen_theme/comments.php
+++ themes/seosen_theme/comments.php
@@ -52,6 +52,7 @@ endif;
          * define twentyeleven_comment() and that will be used instead.
          * See twentyeleven_comment() in twentyeleven/functions.php for more.
          */
+        var_dump(wp_list_comments());
         wp_list_comments();
         ?>
     </ol>
