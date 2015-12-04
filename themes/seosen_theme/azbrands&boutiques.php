3fe9e6502e608882358eaa6926b540bc39dd205e
Kamil Kosakowski <kamilk870310@wp.pl>
2015-12-04 10:23:04 +0100

    adding template for a-z brands

---
+++ themes/seosen_theme/azbrands&boutiques.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Template Name: Brands and boutiques A-Z
+*/
+
+$azbrandsandboutiques = new Template('azbrands&boutiques.php');
+
+echo $azbrandsandboutiques;
---
+++ themes/seosen_theme/views/azbrands&boutiques.php
@@ -0,0 +1,4 @@
+<?php
+get_header();
+echo 'test';
+get_footer();
