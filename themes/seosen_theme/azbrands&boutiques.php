67e52b9ceeae09e885f1203354a677cefdb7bc84
Kamil Kosakowski <kamilk870310@wp.pl>
2015-12-04 12:46:56 +0100

    results print testing in a-z brands task

--- themes/seosen_theme/azbrands&boutiques.php
+++ themes/seosen_theme/azbrands&boutiques.php
@@ -3,6 +3,26 @@
 Template Name: Brands and boutiques A-Z
 */
 
-$azbrandsandboutiques = new Template('azbrands&boutiques.php');
-
-echo $azbrandsandboutiques;
+$tpl = new Template('azbrands&boutiques.php');
+$queryArgs = array(
+    'post_type' => array(
+        'brands',
+        'boutiques'
+    ),
+    'orderby' => 'title',
+    'order' => 'ASC',
+    'nopaging' => true,
+    'post_status' => 'publish'
+);
+$wpQueryObj = new WP_Query($queryArgs);
+$brandsAndBoutiques = $wpQueryObj->get_posts();
+$postsByLetters = array();
+foreach ($brandsAndBoutiques as $oneBrandOrBoutique) {
+    $firstLetter = substr($oneBrandOrBoutique->post_title, 0, 1);
+    if (!key_exists($firstLetter, $postsByLetters)) {
+        $postsByLetters[$firstLetter] = [];
+    }
+    array_push($postsByLetters[$firstLetter], $oneBrandOrBoutique);
+}
+$tpl->postsByLetters = $postsByLetters;
+echo $tpl;
--- themes/seosen_theme/views/azbrands&boutiques.php
+++ themes/seosen_theme/views/azbrands&boutiques.php
@@ -1,4 +1,35 @@
-<?php
-get_header();
-echo 'test';
-get_footer();
+<?php get_header(); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-24" id="content">
+            <?php
+            do_action('ss_css');
+            ?>
+            <h1><span><?php echo get_the_title(); ?></span></h1>
+            <?php
+            if (function_exists('yoast_breadcrumb')) {
+                yoast_breadcrumb('<div id="breadcrumbs">', '</div>');
+            }
+            ?>
+            <?php foreach ($postsByLetters as $letter => $posts): ?>
+                <div class="category_ss_title_under">
+                    <span class="left_ss"> </span>
+
+                    <p class="ss_description category_ss">
+
+                        <b class=""><?php echo $letter; ?></b>
+                    </p>
+
+                    <div class="ss_clear"></div>
+                    <?php foreach ($posts as $onePost): ?>
+                        <div class="col-md-4">
+                            <?php var_dump($onePost); ?>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
